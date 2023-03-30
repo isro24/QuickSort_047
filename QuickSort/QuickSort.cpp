e7a2edb3834a1c51ec342c6bebf54255909811fb
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-03-30 10:39:14 +0700

    Membuat Arraydengan Integers

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// Array ofintegers to hold values
-int 
+// Array of integers to hold values
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
