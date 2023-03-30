0ad6f8f0ad157a821e5769417ab5278f8c452273
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-03-30 11:35:00 +0700

    Menambahkan fungsi q_short() pada program untuk melakukan pengurutan elemen-elemen array menggunakan algoritma quick sort

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -39,4 +39,60 @@ void swap(int x, int y)
 	arr[y] = temp;
 }
 
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)												// Langkah 1
+		return;
+
+	//Partition the list  intotwo parts:
+	//One containing elements less that or equal to pivot
+	//Outher containing elements grather than pivot
+
+	pivot - arr[low];											// Langkah 2			
+
+	i - low + 1;												// Langkah 3
+	j = high;													// Langkah 4
+
+
+	while (i <= j)												// Langkah 10
+	{
+		//Search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))				// Langkah 5
+		{
+			i++;												// Langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//Search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))					// Langkah 7
+		{
+			j--;												// Langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+																// Langkah 9
+		if (i < j) // if the greater element is on the left on the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count;
+		}
+	}
+	//j now containt the index of the element in the sorted list
+	if (low < j)												// Langkah 11
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	q_short(low, j - 1);										// Langkah 12	
+
+	//Sort the list on the right of pivot using quick sort
+	q_short(j + 1, high);										// Langkah 13
+
+
+}
 
