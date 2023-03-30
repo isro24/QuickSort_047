74f12d098acc7c91de9c55ab3fb53c6a2abd14a4
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-03-30 10:53:50 +0700

    Menambahkan fungsi input() untuk meminta input panjang array dan elemennya pada program.

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -7,3 +7,25 @@ int cmp_count = 0;	// number of comparsion
 int mov_count = 0;	// number of data movements
 int n;
 
+void inpur() {
+	while (true)
+	{
+		cout << "Masukkan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksium panjang array adalah 20" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
